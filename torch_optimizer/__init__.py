aa49d7d5e92f27e7f02563b10377630df57b397e
Nikolay Novik <nickolainovik@gmail.com>
2020-01-16 23:10:14 -0500

    Add sgdw optimizer

--- torch_optimizer/__init__.py
+++ torch_optimizer/__init__.py
@@ -4,7 +4,16 @@ from .lookahead import Lookahead
 from .powersign import PowerSign
 from .radam import RAdam
 from .yogi import Yogi
+from .sgdw import SGDW
 
 
-__all__ = ('Lookahead', 'PowerSign', 'DiffGrad', 'AdaMod', 'RAdam', 'Yogi')
+__all__ = (
+    'Lookahead',
+    'PowerSign',
+    'DiffGrad',
+    'AdaMod',
+    'RAdam',
+    'Yogi',
+    'SGDW',
+)
 __version__ = '0.0.1a0'
