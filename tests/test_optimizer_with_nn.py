1676aac07c4c6a05ce321cbdd1ffbef1afd04c64
Nikolay Novik <nickolainovik@gmail.com>
2020-01-20 19:41:59 -0500

    Add simple nn test case

---
+++ tests/test_optimizer_with_nn.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pytest
+import torch
+import torch_optimizer as optim
+
+from torch import nn
+
+
+seed = 42
+torch.manual_seed(seed)
+
+
+def make_dataset(seed=42):
+    rng = np.random.RandomState(seed)
+    N = 100
+    D = 2
+
+    X = rng.randn(N, D) * 2
+
+    # center the first N/2 points at (-2,-2)
+    X[: N // 2, :] = X[: N // 2, :] - 2 * np.ones((N // 2, D))
+
+    # center the last N/2 points at (2, 2)
+    X[N // 2:, :] = X[N // 2 :, :] + 2 * np.ones((N // 2, D))
+
+    # labels: first N/2 are 0, last N/2 are 1
+    Y = np.array([0] * (N // 2) + [1] * (N // 2)).reshape(100, 1)
+
+    x = torch.Tensor(X)
+    y = torch.Tensor(Y)
+    return x, y
+
+
+class LogisticRegression(nn.Module):
+    def __init__(self):
+        super(LogisticRegression, self).__init__()
+        self.linear1 = nn.Linear(2, 4)
+        self.linear2 = nn.Linear(4, 1)
+
+    def forward(self, x):
+        output = torch.relu(self.linear1(x))
+        output = self.linear2(output)
+        y_pred = torch.sigmoid(output)
+        return y_pred
+
+
+def ids(v):
+    n = f'{v[0].__name__} {v[1:]}'
+    return n
+
+
+optimizers = [
+    (optim.SGDW, {'lr': 1.0}, 200),
+    (optim.DiffGrad, {'lr': 0.5}, 200),
+    (optim.AdaMod, {'lr': 2.0}, 200),
+    (optim.Yogi, {'lr': 0.1}, 200),
+]
+
+
+@pytest.mark.parametrize('optimizer_config', optimizers, ids=ids)
+def test_basic_nn_modeloptimizer_config(optimizer_config):
+    x_data, y_data = make_dataset()
+    model = LogisticRegression()
+
+    loss_fn = nn.BCELoss()
+    optimizer_class, config, iterations = optimizer_config
+    optimizer = optimizer_class(model.parameters(), **config)
+
+    for _ in range(iterations):
+        y_pred = model(x_data)
+        loss = loss_fn(y_pred, y_data)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    assert loss.item() < 0.1
