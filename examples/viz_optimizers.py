b631c1d232353fc0f73188a8487b389160850ba4
Yohann Lereclus <lereclus84L@gmail.com>
2020-03-06 11:50:42 +0100

    black format

--- examples/viz_optimizers.py
+++ examples/viz_optimizers.py
@@ -6,15 +6,11 @@ from hyperopt import fmin, tpe, hp
 import torch_optimizer as optim
 from test_functions import Rastrigin, Rosenbrock
 
-plt.style.use('seaborn-white')
+plt.style.use("seaborn-white")
 
 
-def execute_steps(
-        func, initial_state, optimizer_class, optimizer_config, num_iter=500
-):
-    """
-    Execute one steps of the optimizer
-    """
+def execute_steps(func, initial_state, optimizer_class, optimizer_config, num_iter=500):
+    """ Execute one steps of the optimizer """
     x = torch.Tensor(initial_state).requires_grad_(True)
     optimizer = optimizer_class([x], **optimizer_config)
     steps = []
@@ -31,11 +27,9 @@ def execute_steps(
 
 
 def objective(params, func):
-    """
-    Execute objective
-    """
-    lr = params['lr']
-    optimizer_class = params['optimizer_class']
+    """ Execute objective """
+    lr = params["lr"]
+    optimizer_class = params["optimizer_class"]
     minimum = func.minimum
     initial_state = func.initial_state
     optimizer_config = dict(lr=lr)
@@ -47,9 +41,7 @@ def objective(params, func):
 
 
 def plot(func, grad_iter, optimizer_name, lr):
-    """
-    Plot result of a simulation
-    """
+    """ Plot result of a simulation """
     x = torch.linspace(func.x_domain[0], func.x_domain[1], func.num_pt)
     y = torch.linspace(func.y_domain[0], func.y_domain[1], func.num_pt)
     minimum = func.minimum
@@ -62,31 +54,27 @@ def plot(func, grad_iter, optimizer_name, lr):
     fig = plt.figure(figsize=(8, 8))
 
     ax = fig.add_subplot(1, 1, 1)
-    ax.contour(X, Y, Z, func.levels, cmap='jet')
-    ax.plot(iter_x, iter_y, color='r', marker='x')
+    ax.contour(X, Y, Z, func.levels, cmap="jet")
+    ax.plot(iter_x, iter_y, color="r", marker="x")
 
     func_name = func.__name__()
     ax.set_title(
-        f'{func_name} func: {optimizer_name} with {len(iter_x)} '
-        f'iterations, lr={lr:.6}'
+        f"{func_name} func: {optimizer_name} with {len(iter_x)} "
+        f"iterations, lr={lr:.6}"
     )
-    plt.plot(*minimum, 'gD')
-    plt.plot(iter_x[-1], iter_y[-1], 'rD')
-    plt.savefig(f'docs/{func_name}_{optimizer_name}.png')
+    plt.plot(*minimum, "gD")
+    plt.plot(iter_x[-1], iter_y[-1], "rD")
+    plt.savefig(f"docs/{func_name}_{optimizer_name}.png")
 
 
-def execute_experiments(
-        optimizers, func, seed=1
-):
-    """
-    Execute simulation on a list of optimizers using a test function
-    """
+def execute_experiments(optimizers, func, seed=1):
+    """ Execute simulation on a list of optimizers using a test function """
     seed = seed
     for item in optimizers:
         optimizer_class, lr_low, lr_hi = item
         space = {
-            'optimizer_class': hp.choice('optimizer_class', [optimizer_class]),
-            'lr': hp.loguniform('lr', lr_low, lr_hi),
+            "optimizer_class": hp.choice("optimizer_class", [optimizer_class]),
+            "lr": hp.loguniform("lr", lr_low, lr_hi),
         }
         best = fmin(
             fn=lambda x: objective(x, func),
@@ -95,19 +83,15 @@ def execute_experiments(
             max_evals=200,
             rstate=np.random.RandomState(seed),
         )
-        print(best['lr'], optimizer_class)
+        print(best["lr"], optimizer_class)
 
         steps = execute_steps(
-            func,
-            func.initial_state,
-            optimizer_class,
-            {'lr': best['lr']},
-            num_iter=500,
+            func, func.initial_state, optimizer_class, {"lr": best["lr"]}, num_iter=500,
         )
-        plot(func, steps, optimizer_class.__name__, best['lr'])
+        plot(func, steps, optimizer_class.__name__, best["lr"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # python examples/viz_optimizers.py
 
     # Each optimizer has tweaked search space to produce better plots and
@@ -130,6 +114,5 @@ if __name__ == '__main__':
     for test_func in [Rastrigin(), Rosenbrock()]:
         print(f"Test function {test_func.__name__()}")
         execute_experiments(
-            optimizers,
-            test_func,
+            list_optimizers, test_func,
         )
