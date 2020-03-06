bcc38f1ae18bf69c30ee3e207997d0b3a39c1350
Yohann Lereclus <lereclus84L@gmail.com>
2020-03-06 11:11:15 +0100

    remove commented func

--- examples/viz_optimizers.py
+++ examples/viz_optimizers.py
@@ -1,27 +1,16 @@
-import math
+import matplotlib.pyplot as plt
 import numpy as np
-import torch_optimizer as optim
 import torch
 from hyperopt import fmin, tpe, hp
-import matplotlib.pyplot as plt
+
+import torch_optimizer as optim
 from test_functions import *
 
 plt.style.use('seaborn-white')
 
 
-def ackley(tensor, lib=torch):
-    # https://en.wikipedia.org/wiki/Test_functions_for_optimization
-    x, y = tensor
-    f = (
-        -20 * lib.exp(-0.2 * lib.sqrt(0.5 * (x * x + y * y)))
-        - lib.exp(0.5 * lib.cos(2 * math.pi * x) + lib.cos(2 * math.pi * y))
-        + math.e + 20
-    )
-    return f
-
-
 def execute_steps(
-    func, initial_state, optimizer_class, optimizer_config, num_iter=500
+        func, initial_state, optimizer_class, optimizer_config, num_iter=500
 ):
     x = torch.Tensor(initial_state).requires_grad_(True)
     optimizer = optimizer_class([x], **optimizer_config)
@@ -38,45 +27,6 @@ def execute_steps(
     return steps
 
 
-# def objective_rastrigin(params):
-#     lr = params['lr']
-#     optimizer_class = params['optimizer_class']
-#     initial_state = (-2.0, 3.5)
-#     minimum = (0, 0)
-#     optimizer_config = dict(lr=lr)
-#     num_iter = 100
-#     steps = execute_steps(
-#         rastrigin, initial_state, optimizer_class, optimizer_config, num_iter
-#     )
-#     return (steps[0][-1] - minimum[0]) ** 2 + (steps[1][-1] - minimum[1]) ** 2
-#
-#
-# def objective_rosenbrok(params):
-#     lr = params['lr']
-#     optimizer_class = params['optimizer_class']
-#     minimum = (1.0, 1.0)
-#     initial_state = (-2.0, 2.0)
-#     optimizer_config = dict(lr=lr)
-#     num_iter = 100
-#     steps = execute_steps(
-#         rosenbrock, initial_state, optimizer_class, optimizer_config, num_iter
-#     )
-#     return (steps[0][-1] - minimum[0]) ** 2 + (steps[1][-1] - minimum[1]) ** 2
-#
-#
-# def objective_ackley(params):
-#     lr = params['lr']
-#     optimizer_class = params['optimizer_class']
-#     minimum = (0, 0)
-#     initial_state = (-2.0, 4.0)
-#     optimizer_config = dict(lr=lr)
-#     num_iter = 100
-#     steps = execute_steps(
-#         ackley, initial_state, optimizer_class, optimizer_config, num_iter
-#     )
-#     return (steps[0][-1] - minimum[0]) ** 2 + (steps[1][-1] - minimum[1]) ** 2
-
-
 def objective(params, func):
     lr = params['lr']
     optimizer_class = params['optimizer_class']
@@ -85,58 +35,9 @@ def objective(params, func):
     optimizer_config = dict(lr=lr)
     num_iter = 100
     steps = execute_steps(
-        ackley, initial_state, optimizer_class, optimizer_config, num_iter
+        func, initial_state, optimizer_class, optimizer_config, num_iter
     )
     return (steps[0][-1] - minimum[0]) ** 2 + (steps[1][-1] - minimum[1]) ** 2
-
-
-# def plot_rastrigin(grad_iter, optimizer_name, lr):
-#     x = np.linspace(-4.5, 4.5, 250)
-#     y = np.linspace(-4.5, 4.5, 250)
-#     minimum = (0, 0)
-#
-#     X, Y = np.meshgrid(x, y)
-#     Z = rastrigin([X, Y], lib=np)
-#
-#     iter_x, iter_y = grad_iter[0, :], grad_iter[1, :]
-#
-#     fig = plt.figure(figsize=(8, 8))
-#
-#     ax = fig.add_subplot(1, 1, 1)
-#     ax.contour(X, Y, Z, 20, cmap='jet')
-#     ax.plot(iter_x, iter_y, color='r', marker='x')
-#     ax.set_title(
-#         f'Rastrigin func: {optimizer_name} with '
-#         f'{len(iter_x)} iterations, lr={lr:.6}'
-#     )
-#     plt.plot(*minimum, 'gD')
-#     plt.plot(iter_x[-1], iter_y[-1], 'rD')
-#     plt.savefig(f'docs/rastrigin_{optimizer_name}.png')
-#
-#
-# def plot_rosenbrok(grad_iter, optimizer_name, lr):
-#     x = np.linspace(-2, 2, 250)
-#     y = np.linspace(-1, 3, 250)
-#     minimum = (1.0, 1.0)
-#
-#     X, Y = np.meshgrid(x, y)
-#     Z = rosenbrock([X, Y])
-#
-#     iter_x, iter_y = grad_iter[0, :], grad_iter[1, :]
-#
-#     fig = plt.figure(figsize=(8, 8))
-#
-#     ax = fig.add_subplot(1, 1, 1)
-#     ax.contour(X, Y, Z, 90, cmap='jet')
-#     ax.plot(iter_x, iter_y, color='r', marker='x')
-#
-#     ax.set_title(
-#         f'Rosenbrock func: {optimizer_name} with {len(iter_x)} '
-#         f'iterations, lr={lr:.6}'
-#     )
-#     plt.plot(*minimum, 'gD')
-#     plt.plot(iter_x[-1], iter_y[-1], 'rD')
-#     plt.savefig(f'docs/rosenbrock_{optimizer_name}.png')
 
 
 def plot(func, grad_iter, optimizer_name, lr):
@@ -144,7 +45,7 @@ def plot(func, grad_iter, optimizer_name, lr):
     y = torch.linspace(func.y_domain[0], func.y_domain[1], func.num_pt)
     minimum = func.minimum
 
-    Y, X = torch.meshgrid(x, y)
+    X, Y = torch.meshgrid(x, y)
     Z = func([X, Y])
 
     iter_x, iter_y = grad_iter[0, :], grad_iter[1, :]
@@ -166,7 +67,7 @@ def plot(func, grad_iter, optimizer_name, lr):
 
 
 def execute_experiments(
-    optimizers, func, seed=1
+        optimizers, func, seed=1
 ):
     seed = seed
     for item in optimizers:
