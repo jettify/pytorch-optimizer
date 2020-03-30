cd7ec3792eb85a462800295348c3e9aa308f84fc
mpariente <pariente.mnl@gmail.com>
2020-03-30 09:29:00 +0200

    Release py3.5 pytorch_ranger release 0.1.0
    (cherry picked from commit ac318f2e65da1b9695c1483fc888ef734f061ba5)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 
-install_requires = ['torch>=1.1.0', 'pytorch_ranger>=0.0.6']
+install_requires = ['torch>=1.1.0', 'pytorch_ranger>=0.1.0']
 
 PY35 = (3, 5, 0)
 
