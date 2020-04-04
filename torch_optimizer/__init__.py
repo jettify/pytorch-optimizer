d5c354adf0678df80851b23c86a8a44d0802e26b
mpariente <pariente.mnl@gmail.com>
2020-04-04 14:50:09 +0200

    Update optim.get method

--- torch_optimizer/__init__.py
+++ torch_optimizer/__init__.py
@@ -1,3 +1,4 @@
+from typing import Optional, Union, Callable, Type
 from .accsgd import AccSGD
 from .adabound import AdaBound
 from .adamod import AdaMod
@@ -12,6 +13,7 @@ from .radam import RAdam
 from .sgdw import SGDW
 from .yogi import Yogi
 from pytorch_ranger import Ranger, RangerQH, RangerVA
+from torch import optim
 
 
 __all__ = (
@@ -35,27 +37,55 @@ __all__ = (
 __version__ = '0.0.1a11'
 
 
-def get(identifier):
-    """ Returns an optimizer class from a string. Returns `identifier` if it
-    is already callable.
+package_opts = [
+    AccSGD,
+    AdaBound,
+    AdaMod,
+    DiffGrad,
+    Lamb,
+    Lookahead,
+    NovoGrad,
+    PID,
+    QHAdam,
+    QHM,
+    RAdam,
+    SGDW,
+    Yogi,
+    Ranger,
+    RangerQH,
+    RangerVA,
+]
+
+builtin_opts = [
+    optim.Adadelta,
+    optim.Adagrad,
+    optim.Adam,
+    optim.AdamW,
+    optim.SparseAdam,
+    optim.Adamax,
+    optim.ASGD,
+    optim.SGD,
+    optim.Rprop,
+    optim.RMSprop,
+    optim.LBFGS
+]
+
+NAME_OPTIM_MAP = {
+    opt.__name__.lower(): opt for opt in package_opts + builtin_opts
+}
+
+
+def get(name: str,) -> Optional[Type[optim.Optimizer]]:
+    r"""Returns an optimizer class from its name. Case insensitive.
 
     Args:
-        identifier (Union[str, Callable, None]): the optimizer identifier.
-
-    Returns:
-        torch.optim.Optimizer or None
+        name: the optimizer name.
     """
-    if identifier is None:
-        return None
-    elif callable(identifier):
-        return identifier
-    elif isinstance(identifier, str):
-        cls = globals().get(identifier)
+    if isinstance(name, str):
+        cls = NAME_OPTIM_MAP.get(name.lower())
         if cls is None:
-            raise ValueError('Could not interpret optimizer identifier: ' +
-                             str(identifier))
+            raise ValueError('Could not interpret optimizer name: ' +
+                             str(name))
         return cls
-    else:
-        raise ValueError('Could not interpret optimizer identifier: ' +
-                         str(identifier))
-
+    raise ValueError('Could not interpret optimizer name: ' +
+                     str(name))
