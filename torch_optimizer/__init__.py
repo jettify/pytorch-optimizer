4376be147271bcae986eaa43a23cfc5f299de9ea
Pariente Manuel <pariente.mnl@gmail.com>
2020-04-04 20:30:33 +0200

    Add torch_optimizer.get() method (#95)
    * Add torch_optimizer.get method
    
    * Update optim.get method
    
    * Test optim.get method
    
    * Hopefully fix CI

--- torch_optimizer/__init__.py
+++ torch_optimizer/__init__.py
@@ -1,3 +1,4 @@
+from typing import Optional, Type
 from .accsgd import AccSGD
 from .adabound import AdaBound
 from .adamod import AdaMod
@@ -12,6 +13,7 @@ from .radam import RAdam
 from .sgdw import SGDW
 from .yogi import Yogi
 from pytorch_ranger import Ranger, RangerQH, RangerVA
+from torch import optim
 
 
 __all__ = (
@@ -33,3 +35,57 @@ __all__ = (
     'RangerVA'
 )
 __version__ = '0.0.1a11'
+
+
+package_opts = [
+    AccSGD,
+    AdaBound,
+    AdaMod,
+    DiffGrad,
+    Lamb,
+    Lookahead,
+    NovoGrad,
+    PID,
+    QHAdam,
+    QHM,
+    RAdam,
+    SGDW,
+    Yogi,
+    Ranger,
+    RangerQH,
+    RangerVA,
+]
+
+builtin_opts = [
+    optim.Adadelta,
+    optim.Adagrad,
+    optim.Adam,
+    optim.AdamW,
+    optim.SparseAdam,
+    optim.Adamax,
+    optim.ASGD,
+    optim.SGD,
+    optim.Rprop,
+    optim.RMSprop,
+    optim.LBFGS
+]
+
+NAME_OPTIM_MAP = {
+    opt.__name__.lower(): opt for opt in package_opts + builtin_opts
+}
+
+
+def get(name: str,) -> Optional[Type[optim.Optimizer]]:
+    r"""Returns an optimizer class from its name. Case insensitive.
+
+    Args:
+        name: the optimizer name.
+    """
+    if isinstance(name, str):
+        cls = NAME_OPTIM_MAP.get(name.lower())
+        if cls is None:
+            raise ValueError('Could not interpret optimizer name: ' +
+                             str(name))
+        return cls
+    raise ValueError('Could not interpret optimizer name: ' +
+                     str(name))
