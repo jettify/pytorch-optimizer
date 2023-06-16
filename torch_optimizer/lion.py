a7348c646b5d7eefc8529a6e2ba57368ca6dfb32
Jona Sassenhagen <jona.sassenhagen@gmail.com>
2023-06-16 21:24:33 +0200

    Update lion.py

--- torch_optimizer/lion.py
+++ torch_optimizer/lion.py
@@ -45,7 +45,7 @@ class Lion(Optimizer):
         weight_decay: float = 0.0,
     ):
 
-        if not 0.0 <= lr:
+        if lr <= 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= betas[0] < 1.0:
             raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
