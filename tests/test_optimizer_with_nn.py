43756ff40c1093ee85c64bc5578e6479a1884df6
Jona Sassenhagen <jona.sassenhagen@gmail.com>
2023-06-16 21:20:37 +0200

    Update test_optimizer_with_nn.py

--- tests/test_optimizer_with_nn.py
+++ tests/test_optimizer_with_nn.py
@@ -1,114 +1,162 @@
-import numpy as np
 import pytest
 import torch
-from torch import nn
 
 import torch_optimizer as optim
 
 
-def make_dataset(seed=42):
-    rng = np.random.RandomState(seed)
-    N = 100
-    D = 2
+def assert_sparse_not_supported(optimizer_class, err_msg=None):
+    param = torch.randn(1, 1).to_sparse().requires_grad_(True)
+    grad = torch.randn(1, 1).to_sparse()
+    param.grad = grad
+    optimizer = optimizer_class([param])
+    optimizer.zero_grad()
+    with pytest.raises(RuntimeError) as ctx:
+        optimizer.step()
 
-    X = rng.randn(N, D) * 2
-
-    # center the first N/2 points at (-2,-2)
-    mid = N // 2
-    X[:mid, :] = X[:mid, :] - 2 * np.ones((mid, D))
-
-    # center the last N/2 points at (2, 2)
-    X[mid:, :] = X[mid:, :] + 2 * np.ones((mid, D))
-
-    # labels: first N/2 are 0, last N/2 are 1
-    Y = np.array([0] * mid + [1] * mid).reshape(100, 1)
-
-    x = torch.Tensor(X)
-    y = torch.Tensor(Y)
-    return x, y
+    msg = err_msg or 'does not support sparse gradients'
+    assert msg in str(ctx.value)
 
 
-class LogisticRegression(nn.Module):
-    def __init__(self):
-        super(LogisticRegression, self).__init__()
-        self.linear1 = nn.Linear(2, 4)
-        self.linear2 = nn.Linear(4, 1)
-
-    def forward(self, x):
-        output = torch.relu(self.linear1(x))
-        output = self.linear2(output)
-        y_pred = torch.sigmoid(output)
-        return y_pred
-
-
-def ids(v):
-    return '{} {}'.format(v[0].__name__, v[1:])
-
-
-def build_lookahead(*a, **kw):
-    base = optim.Yogi(*a, **kw)
-    return optim.Lookahead(base)
-
-
-optimizers = [
-    (build_lookahead, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.A2GradExp, {'lips': 2.0, 'beta': 1e-3}, 500),
-    (optim.A2GradInc, {'lips': 5.0, 'beta': 1e-3}, 200),
-    (optim.A2GradUni, {'lips': 5.0, 'beta': 1e-3}, 500),
-    (optim.AccSGD, {'lr': 1.0, 'weight_decay': 1e-3}, 200),
-    (optim.AdaBelief, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.AdaBound, {'lr': 1.5, 'gamma': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.AdaMod, {'lr': 2.0, 'weight_decay': 1e-3}, 200),
-    (optim.Adafactor, {'lr': 0.004466, 'weight_decay': 1e-3}, 1500),
-    (optim.AdamP, {'lr': 0.045, 'weight_decay': 1e-3}, 800),
-    (optim.AggMo, {'lr': 0.17059, 'weight_decay': 1e-3}, 1000),
-    (optim.Apollo, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.DiffGrad, {'lr': 0.5, 'weight_decay': 1e-3}, 200),
-    (
-        optim.LARS,
-        {'lr': 1.0, 'weight_decay': 1e-3, 'trust_coefficient': 0.01},
-        200,
-    ),
-    (optim.Lamb, {'lr': 0.0151, 'weight_decay': 1e-3}, 1000),
-    (optim.MADGRAD, {'lr': 1.0, 'weight_decay': 1e-3}, 200),
-    (optim.NovoGrad, {'lr': 0.01, 'weight_decay': 1e-3}, 200),
-    (optim.PID, {'lr': 0.01, 'weight_decay': 1e-3, 'momentum': 0.1}, 200),
-    (optim.QHAdam, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.QHM, {'lr': 0.1, 'weight_decay': 1e-5, 'momentum': 0.2}, 200),
-    (optim.RAdam, {'lr': 1.0, 'weight_decay': 1e-3}, 200),
-    (optim.Ranger, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.RangerQH, {'lr': 0.0124, 'weight_decay': 1e-3}, 1100),
-    (optim.RangerVA, {'lr': 0.2214, 'weight_decay': 1e-3}, 500),
-    (optim.SGDP, {'lr': 1.0, 'weight_decay': 1e-3}, 200),
-    (optim.SGDW, {'lr': 1.0, 'weight_decay': 1e-3}, 200),
-    (optim.SWATS, {'lr': 0.703, 'weight_decay': 1e-3}, 600),
-    (
-        optim.Shampoo,
-        {'lr': 0.279, 'weight_decay': 1e-3, 'momentum': 0.05},
-        1600,
-    ),
-    (optim.Yogi, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.Adahessian, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
-    (optim.Lion, {'lr': 0.1, 'weight_decay': 1e-3}, 200),
+no_sparse_optimizers = [
+    optim.AdaBound,
+    optim.AdaMod,
+    optim.DiffGrad,
+    optim.Lamb,
+    optim.NovoGrad,
+    optim.RAdam,
+    optim.Yogi,
 ]
 
 
-@pytest.mark.parametrize('optimizer_config', optimizers, ids=ids)
-def test_basic_nn_modeloptimizer_config(optimizer_config):
-    torch.manual_seed(42)
-    x_data, y_data = make_dataset()
-    model = LogisticRegression()
+@pytest.mark.parametrize('optimizer_class', no_sparse_optimizers)
+def test_sparse_not_supported(optimizer_class):
+    assert_sparse_not_supported(optimizer_class)
 
-    loss_fn = nn.BCELoss()
-    optimizer_class, config, iterations = optimizer_config
-    optimizer = optimizer_class(model.parameters(), **config)
-    init_loss = None
-    for _ in range(iterations):
-        y_pred = model(x_data)
-        loss = loss_fn(y_pred, y_data)
-        if init_loss is None:
-            init_loss = loss
-        optimizer.zero_grad()
-        loss.backward(create_graph=True)
-        optimizer.step()
-    assert init_loss.item() > 2.0 * loss.item()
+
+optimizers = [
+    optim.AccSGD,
+    optim.AdaBelief,
+    optim.AdaBound,
+    optim.AdaMod,
+    optim.AdamP,
+    optim.AggMo,
+    optim.Apollo,
+    optim.DiffGrad,
+    optim.LARS,
+    optim.Lamb,
+    optim.MADGRAD,
+    optim.NovoGrad,
+    optim.PID,
+    optim.QHAdam,
+    optim.QHM,
+    optim.RAdam,
+    optim.SGDP,
+    optim.SGDW,
+    optim.SWATS,
+    optim.Shampoo,
+    optim.Yogi,
+    optim.Lion,
+]
+
+
+@pytest.mark.parametrize('optimizer_class', optimizers)
+def test_learning_rate(optimizer_class):
+    lr = -0.01
+    with pytest.raises(ValueError) as ctx:
+        optimizer_class(None, lr=-0.01)
+    msg = 'Invalid learning rate: {}'.format(lr)
+    assert msg in str(ctx.value)
+
+
+eps_optimizers = [
+    optim.AdaBelief,
+    optim.AdaBound,
+    optim.AdaMod,
+    optim.AdamP,
+    optim.Apollo,
+    optim.DiffGrad,
+    optim.LARS,
+    optim.Lamb,
+    optim.MADGRAD,
+    optim.NovoGrad,
+    optim.QHAdam,
+    optim.RAdam,
+    optim.SGDP,
+    optim.SWATS,
+    optim.Yogi,
+]
+
+
+@pytest.mark.parametrize('optimizer_class', eps_optimizers)
+def test_eps_validation(optimizer_class):
+    eps = -0.1
+    with pytest.raises(ValueError) as ctx:
+        optimizer_class(None, lr=0.1, eps=eps)
+    msg = 'Invalid epsilon value: {}'.format(eps)
+    assert msg in str(ctx.value)
+
+
+weight_decay_optimizers = [
+    optim.AccSGD,
+    optim.AdaBelief,
+    optim.AdaBound,
+    optim.AdaMod,
+    optim.Adafactor,
+    optim.AdamP,
+    optim.AggMo,
+    optim.Apollo,
+    optim.DiffGrad,
+    optim.LARS,
+    optim.Lamb,
+    optim.MADGRAD,
+    optim.NovoGrad,
+    optim.PID,
+    optim.QHAdam,
+    optim.QHM,
+    optim.RAdam,
+    optim.SGDP,
+    optim.SGDW,
+    optim.SWATS,
+    optim.Shampoo,
+    optim.Yogi,
+    optim.Lion,
+]
+
+
+@pytest.mark.parametrize('optimizer_class', weight_decay_optimizers)
+def test_weight_decay_validation(optimizer_class):
+    weight_decay = -0.1
+    with pytest.raises(ValueError) as ctx:
+        optimizer_class(None, lr=0.1, weight_decay=weight_decay)
+    msg = 'Invalid weight_decay value: {}'.format(weight_decay)
+    assert msg in str(ctx.value)
+
+
+betas_optimizers = [
+    optim.AdaBelief,
+    optim.AdaBound,
+    optim.AdaMod,
+    optim.AdamP,
+    optim.DiffGrad,
+    optim.Lamb,
+    optim.NovoGrad,
+    optim.QHAdam,
+    optim.RAdam,
+    optim.Yogi,
+    optim.Lion,
+]
+
+
+@pytest.mark.parametrize('optimizer_class', betas_optimizers)
+def test_betas_validation(optimizer_class):
+    betas = (-1, 0.999)
+    with pytest.raises(ValueError) as ctx:
+        optimizer_class(None, lr=0.1, betas=(-1, 0.999))
+    msg = 'Invalid beta parameter at index 0: {}'.format(betas[0])
+    assert msg in str(ctx.value)
+
+    betas = (0.9, -0.999)
+    with pytest.raises(ValueError) as ctx:
+        optimizer_class(None, lr=0.1, betas=betas)
+    msg = 'Invalid beta parameter at index 1: {}'.format(betas[1])
+    assert msg in str(ctx.value)
